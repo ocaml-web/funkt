d95ff97622c2af5d0bc7fef00581beeb4e505b10
Cuihtlauac ALVARADO <cuihtmlauac@tarides.com>
2023-12-06 16:36:49 +0100

    Example executable

--- funkt.ml
+++ funkt.ml
@@ -1,1 +1,7 @@
 module StringSet = Set.Make(String)
+
+let _ =
+  In_channel.input_lines stdin
+  |> List.concat_map Str.(split (regexp "[ \t.,;:()]+"))
+  |> StringSet.of_list
+  |> StringSet.iter print_endline
